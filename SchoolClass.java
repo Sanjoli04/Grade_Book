f32aba19867ad3b40122e8bd361b22f1f663e99c
Sanjoli Vashisth <vashisthsanjoli@gmail.com>
2024-10-22 17:06:28 +0530

    Update SchoolClass.java

--- SchoolClass.java
+++ SchoolClass.java
@@ -2,48 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SchoolClass {
-//    classNum,section
+    // class Num and subjects??
+    public static List<SchoolClass> classes;
     private int classNum;
-    private List<String> sections;
-    private List<Student> students;
-    public ArrayList<String> getSections() {
-        return (ArrayList<String>) sections;
+    private List<String> subjects;
+    public static      void __init__(){
+        classes = new ArrayList<>();
+    }
+    public SchoolClass(int classNum, List<String> subjects){
+        this.classNum = classNum;
+        this.subjects = subjects;
+        classes.add(this);
+    }
+
+    public void setSubjects(List<String> subjects) {
+        this.subjects = subjects;
+    }
+
+    public int getClassNum() {
+        return classNum;
     }
 
     public void setClassNum(int classNum) {
         this.classNum = classNum;
     }
 
-    public void setSection(List<String> sections) {
-        this.sections = sections;
-    }
-
-    public int getClassNum() {
-        return classNum;
-    }
-    SchoolClass(int classNum,List<String> sections){
-        this.classNum = classNum;
-        this.sections = sections;
-        this.students = new ArrayList<>(50);
-    }
-
     @Override
-    public boolean equals(Object obj) {
-        if(obj instanceof SchoolClass){
-            SchoolClass schoolClass = (SchoolClass) obj;
-            if(schoolClass.classNum == this.classNum && schoolClass.sections.equals(this.sections)){
-                return true;
-            }
-            return false;
-        }
-        return false;
+    public String toString() {
+        return "SchoolClass{" +
+                "classNum=" + classNum +
+                '}';
     }
 
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<Student> students) {
-        this.students = students;
-    }
 }
